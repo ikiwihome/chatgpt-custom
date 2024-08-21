d330b90ba56a058104ec496463554e1e9b625cdc
ikiwi <ikiwicc@gmail.com>
2024-08-22 02:38:31 +0800

    feat:upgrade to 3.0.0 at 20240821

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -45,6 +45,39 @@ let api: ChatGPTAPI | ChatGPTUnofficialProxyAPI
       debug: !disableDebug,
     }
 
+    // Set the token limits based on the model's type. This is because different models have different token limits.
+    // The token limit includes the token count from both the message array sent and the model response.
+
+    // Check if the model type is GPT-4o-mini
+    if (model.toLowerCase().includes('gpt-4o-mini')) {
+      // If it's a 'gpt-4o'/'gpt-4-turbo'/'xxxx-preview' model, set the maxModelTokens to 128000
+      options.maxModelTokens = 128000
+      options.maxResponseTokens = 16384
+    }
+    // Check if the model type is GPT-4o
+    if (model.toLowerCase().includes('gpt-4o')) {
+      // If it's a 'gpt-4o'/'gpt-4-turbo'/'xxxx-preview' model, set the maxModelTokens to 128000
+      options.maxModelTokens = 128000
+      options.maxResponseTokens = 4096
+    }
+    // Check if the model type is GPT-4-turbo or newer
+    else if (model.toLowerCase().includes('gpt-4')) {
+      // If it's a 'gpt-4' model, set the maxModelTokens and maxResponseTokens to 8192 and 2048 respectively
+      options.maxModelTokens = 8192
+      options.maxResponseTokens = 2048
+    }
+    // Check if the model type includes 'gpt-3.5-turbo'
+    else if (model.toLowerCase().includes('gpt-3.5-turbo')) {
+      // If it's a 'gpt-3.5-turbo' model, set the maxModelTokens to 16385 and maxResponseTokens to 4096
+      options.maxModelTokens = 16385
+      options.maxResponseTokens = 4096
+    }
+    // If none of the above, use the default values
+    else {
+      options.maxModelTokens = 4096
+      options.maxResponseTokens = 1024
+    }
+
     if (isNotEmptyString(OPENAI_API_BASE_URL))
       options.apiBaseUrl = `${OPENAI_API_BASE_URL}/v1`
 
